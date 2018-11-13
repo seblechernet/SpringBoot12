b34bc3f94c27af3fb61c91ec97a7b80f803fd3a5
seblechernet <seble.t.chernet@gmail.com>
2018-11-13 01:07:14 -0500

    Initial commit

---
+++ src/main/java/com/seb/springboot12/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.seb.springboot12;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor,Long> {
+}
---
+++ src/main/java/com/seb/springboot12/CloudinaryConfig.java
@@ -0,0 +1,40 @@
+package com.seb.springboot12;
+
+import org.apache.tomcat.websocket.Transformation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+public class CloudinaryConfig {
+    private Cloudinary cloudinary;
+
+    @Autowired
+    public CloudinaryConfig(@Value("${cloud.key}") String key, @Value("${cloud.secret}") String secret, @Value("${cloud.name}") String cloud) {
+        cloudinary = Singleton.getCloudinary();
+        cloudinary.config.cloudName = cloud;
+        cloudinary.config.apiSecret = secret;
+        cloudinary.config.apiKey = key;
+    }
+
+    public Map upload(Object file, Map options) {
+        try {
+            return clcoudinary.upload().upload(file, options);
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String createUrl(String name,int width,int height,String action){
+        return cloudinary.url()
+                .transformation(new Transformation())
+                .width(width).height(height)
+                .border("2px_solid_black").crop(action)
+                .imageTag(name);
+    }
+}
